8566f7c4fd11e0ca1b4e69a882528098977effcb
G00DCOD3R <82707639+G00DCOD3R@users.noreply.github.com>
2021-07-20 16:14:28 +0200

    new functions

--- a.cpp
+++ a.cpp
@@ -28,16 +28,21 @@ int random(int a, int b)
 
 vector <PII> deck;
 
-void __init();
-void show_card(PII & a);
-void show_deck(const vector <PII> & deck);
-bool check(ll cards, int hand);
-ll conv(const vector <PII> & v);
-void show_int(ll x);
+void __init(); // initialize deck of cards
+void show_card(PII & a); // show single card
+void show_deck(const vector <PII> & deck); // shows deck of cards
+bool check(ll cards, int hand); // checks if hand exist in cards (cards is mask, hand is code from 0 to 381
+ll cards_to_mask(const vector <PII> & v); // converts cards to mask
+void show_int(ll x); // shows long long x in binary form
+int name_to_code(); // converts name of hand to its code
+void code_to_name(int code); // converts code of hand to its name (writes it)
+
+
 
 int main()
 {
 	__init();
+	
 	
 	//  while(true)
 	//  {
@@ -73,6 +78,7 @@ void __init()
 		for(int j=0;j<4;j++) deck.pb(mp(i, j));
 	}
 }
+
 void show_card(PII & a)
 {
 	// showing cards 2 - 14 (higher than 10 are J, Q, K, A. 
@@ -88,17 +94,20 @@ void show_card(PII & a)
 	vector <char> color {'C', 'D', 'H', 'S'};
 	printf("%c ", color[a.se]);
 }
+
 void show_deck(const vector <PII> & deck)
 {
 	for(auto e: deck) show_card(e);
 	puts("");
 }
-ll conv(const vector <PII> & v)
+
+ll cards_to_mask(const vector <PII> & v)
 {
 	ll w = 0;
 	for(auto &e: v) w |= ((1LL << e.se) << ((e.fi - 2) * 4));
 	return w;
 }
+
 void show_int(ll x)
 {
 	for(int i=0;i<52;i++)
@@ -108,6 +117,7 @@ void show_int(ll x)
 	}
 	puts("");
 }
+
 bool check(ll cards, int hand)
 {
 	// cards is mask of cards in order: 
@@ -128,7 +138,8 @@ bool check(ll cards, int hand)
 		ll mask = 15;
 		mask <<= (hand - 13) * 4;
 		mask = (cards&mask);
-		if(ppc(mask) >= 2) return true;
+		int tmp = ppc(mask);
+		if(tmp >= 2) return true;
 		return false;
 		
 	}
@@ -153,8 +164,8 @@ bool check(ll cards, int hand)
 		hand -= 182;
 		ll mask = (15LL << (hand * 4));
 		mask &= cards;
-		if(ppc(mask) >= 3) return true;
-		return false;
+		if(ppc(mask) < 3) return false;
+		return true;
 	}
 	else if(hand < 204)
 	{
@@ -191,8 +202,10 @@ bool check(ll cards, int hand)
 		hand -= 360;
 		ll mask = (15LL << (hand * 4));
 		mask &= cards;
-		if(ppc(mask) >= 4) return true;
-		return false;
+		//  if(ppc(mask) >= 4) return true;
+		//  return false;
+		if(ppc(mask) < 4) return false;
+		return true;
 	}
 	else if(hand < 382)
 	{
@@ -218,4 +231,182 @@ bool check(ll cards, int hand)
 	}
 	puts("CHECKING NON-EXISTING HAND");
 	return false;
+}
+
+int name_to_code()
+{
+	// available names: (in polish)
+	
+	//  jedna X
+	//  para X
+	//  dwie X Y
+	//  trzy X
+	//  strit X
+	//  full X Y
+	//  kareta X
+	//  poker X
+	
+	
+	string s;
+	while(true)
+	{
+		cin >> s;
+		string type;
+		cin >> type;
+		vector <string> opts {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		int who = -1;
+		for(int i=0;i<opts.size();i++)
+		{
+			if(opts[i] == type)
+			{
+				who = i;
+				break;
+			}
+		}
+		if(who == -1)
+		{
+			puts("No such card!");
+			continue;
+		}
+		if(s == "jedna")
+		{
+			return who;
+		}
+		if(s == "para")
+		{
+			return 13 + who;
+		}
+		if(s == "dwie")
+		{
+			cin >> type;
+			int sec = -1;
+			for(int i=0;i<opts.size();i++)
+			{
+				if(opts[i] == type)
+				{
+					sec = i;
+					break;
+				}
+			}
+			if(sec == -1) 
+			{
+				puts("No such card (second)!");
+				continue;
+			}
+			if(sec > who) sec--;
+			return 26 + who * 12 + sec;
+		}
+		if(s == "trzy")
+		{
+			return 182 + who;
+		}
+		if(s == "strit")
+		{
+			if(who > 8) 
+			{
+				puts("No such strit");
+				continue;
+			}
+			return 195 + who;
+		}
+		if(s == "full")
+		{
+			cin >> type;
+			int sec = -1;
+			for(int i=0;i<opts.size();i++)
+			{
+				if(opts[i] == type)
+				{
+					sec = i;
+					break;
+				}
+			}
+			if(sec == -1) 
+			{
+				puts("No such card (second)!");
+				continue;
+			}
+			if(sec > who) sec--;
+			return 204 + who * 12 + sec;
+		}
+		if(s == "kareta")
+		{
+			return 360 + who;
+		}
+		if(s == "poker")
+		{
+			if(who > 8)
+			{
+				puts("No such poker");
+				continue;
+			}
+			return 373 + who;
+		}
+		puts("no such poker hand\ntry for example:");
+		printf("jedna X\npara X\ndwie X Y\ntrzy X\nstrit X\nfull X Y\nkareta X\npoker X\n\nWhere X Y signify cards\n");
+	}
+	
+}
+void code_to_name(int code)
+{
+	vector <string> opts {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+	if(code < 0) 
+	{
+		cout << "no such code (" << code << ")\n";
+		return;
+	}
+	if(code < 13)
+	{
+		// highest card;
+		cout << "jedna " << opts[code] << "\n";
+		
+	}
+	else if(code < 26)
+	{
+		// pair
+		cout << "para " << opts[code - 13] << "\n";
+	}
+	else if(code < 182)
+	{
+		// double pair
+		code -= 26;
+		int a = code / 12;
+		int b = code - a * 12;
+		if(b >= a) b++;
+		cout << "dwie " << opts[a] << " " << opts[b] << "\n";
+	}
+	else if(code < 195)
+	{
+		// three of a kind
+		code -= 182;
+		cout << "trzy " << opts[code] << "\n";
+	}
+	else if(code < 204)
+	{
+		// straight
+		code -= 195;
+		cout << "strit " << opts[code] << "\n";
+	}
+	else if(code < 360)
+	{
+		// full house
+		code -= 204;
+		int a = code / 12;
+		int b = code - a * 12;
+		if(b >= a) b++;
+		cout << "full " << opts[a] << " " << opts[b] << "\n";
+		
+	}
+	else if(code < 373)
+	{
+		// four of a kind
+		code -= 360;
+		cout << "kareta " << opts[code] << "\n";
+	}
+	else if(code < 382)
+	{
+		// straight flush
+		code -= 373;
+		cout << "pokerzysko " << opts[code] << "\n";
+	}
 }
